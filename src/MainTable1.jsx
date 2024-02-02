aededd7dca94c2faacc11b6e1d1a988136a39bec
Gimena Motto <gimenapimba@gmail.com>
2024-02-02 10:10:11 +0100

    add filterData

--- src/MainTable1.jsx
+++ src/MainTable1.jsx
@@ -5,6 +5,7 @@ import './MainTable.css';
 
 const MainTable1 = () => {
     const [data, setData] = useState([]);
+    const [formattedToday, setFormattedToday] = useState("");
 
     useEffect(() => {
         const fetchData = async () => {
@@ -16,41 +17,48 @@ const MainTable1 = () => {
 
                 Papa.parse(csvData, {
                     complete: function (result) {
-                        // El resultado es un objeto con propiedades "data", "errors", etc.
-                        // En "data" tendrás la matriz de datos CSV
                         setData(result.data);
                     },
-                    header: true // Si tu archivo CSV tiene encabezados de columna, establece esto en true
+                    header: true
                 });
             } catch (error) {
                 console.error('Error al cargar los datos:', error);
             }
         };
 
-
         fetchData();
+
+        const today = new Date();
+        const formattedDate = `${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`;
+        setFormattedToday(formattedDate);
+
     }, []);
 
-    // Filtra las filas para mostrar solo aquellas en las que la sexta columna tenga el valor "CESAR"
-    const dataFiltrado = data.filter((row, index) => index === 1 || row[6] === 'CESAR');
-
-    console.log('Datos en el estado:', data);
+    // Filtra las filas para mostrar la primera fila siempre en negrita y las filas donde el mensajero sea "CESAR" y la fecha sea la de hoy
+    const filterData = data.filter((row, index) => {
+        if (index === 0 || (row['MENSAJERX'] === 'CESAR' && row['FECHA'] === formattedToday)) {
+            return true; // Mantener la primera fila siempre y las filas que cumplen la condición
+        }
+        return false; // Omitir las filas que no cumplen la condición
+    });
 
     return (
-        <div>
+        <div className="main-table-container">
             <h2>Pedidos filtrados CESAR de tabla general</h2>
             <table className="main-table">
                 <thead>
                     <tr>
-                        {data.length > 0 && Object.keys(data[0]).map((key, index) => (
-                            <th key={index}>{key}</th>
+                        {/* Solo mostramos las primeras 26 columnas */}
+                        {data.length > 0 && Object.keys(data[0]).slice(0, 26).map((key, index) => (
+                            <th key={index} className={index === 0 ? "bold-row" : ""}>{key}</th>
                         ))}
                     </tr>
                 </thead>
                 <tbody>
-                    {data.map((row, rowIndex) => (
+                    {filterData.map((row, rowIndex) => (
                         <tr key={rowIndex}>
-                            {Object.values(row).map((cell, cellIndex) => (
+                            {/* Mostramos todas las columnas */}
+                            {Object.values(row).slice(0, 26).map((cell, cellIndex) => (
                                 <td key={cellIndex}>{cell}</td>
                             ))}
                         </tr>
@@ -62,3 +70,4 @@ const MainTable1 = () => {
 };
 
 export default MainTable1;
+
